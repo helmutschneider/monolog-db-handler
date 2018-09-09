422f6dc45e0a345021801343103bc2f197d726b4
Johan Björk <johanimon@gmail.com>
2018-09-09 17:48:27 +0200

    Fix sqlite data types

--- schema/sqlite.sql
+++ schema/sqlite.sql
@@ -5,6 +5,6 @@ CREATE TABLE "log" (
     "level" INTEGER NOT NULL,
     "datetime" TEXT NOT NULL,
     "message" BLOB NOT NULL,
-    "context" LONGBLOB NOT NULL,
-    "extra" LONGBLOB NOT NULL
+    "context" BLOB NOT NULL,
+    "extra" BLOB NOT NULL
 );
